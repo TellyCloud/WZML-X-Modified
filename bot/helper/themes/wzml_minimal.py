b8d12e28ddc1d2d58bed411a1f1271206b0fab56
TellyCloud <156745846+TellyCloud@users.noreply.github.com>
2024-10-01 16:38:44 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -171,7 +171,7 @@ class WZMLStyle:
     L_CC              ='<b><code>👤User By : </code>{Tag}</b>\n\n'
     PM_BOT_MSG        ='<blockquote>➲ <b><i>File(s) have been Sent above</i></b></blockquote>'
     L_BOT_MSG         ='<blockquote>➲ <b><i>File(s) have been Sent to Bot PM (Private)</i></b></blockquote>'
-    L_LL_MSG          ='<blockquote>➲ <b><i> LOVE YOU </i></b></blockquote>\n'
+    L_LL_MSG          ='<blockquote>➲ <b><i>File(s) have been Sent. Access via Links...</i></b></blockquote>\n'
     
     # ----- MIRROR -------
     M_TYPE            ='<b><code>🔡Type     : </code>{Mimetype}</b>\n'
